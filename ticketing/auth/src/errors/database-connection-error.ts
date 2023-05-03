ef4f2b41c57f5a5a1215db13f0849d8c3d56332f
seanarch <zhuqingyun@outlook.com>
2023-05-03 16:18:40 -0600

    Update error and message

--- ticketing/auth/src/errors/database-connection-error.ts
+++ ticketing/auth/src/errors/database-connection-error.ts
@@ -1,6 +1,8 @@
 import { ValidationError } from "express-validator";
 
 export class DatabaseConnectionError extends Error {
+  reason = "Error connecting to database";
+
   constructor(public errors: ValidationError[]) {
     super();
 
--- ticketing/auth/src/middlewares/error-handler.ts
+++ ticketing/auth/src/middlewares/error-handler.ts
@@ -9,14 +9,17 @@ export const errorHandler = (
   next: NextFunction
 ) => {
   if (err instanceof RequestValidationError) {
-    console.log("handling as request validation error");
+    const formattedErrors = err.errors.map((error) => {
+      return { message: error.msg };
+    });
+    return res.status(400).send({ errors: formattedErrors });
   }
 
   if (err instanceof DatabaseConnectionError) {
-    console.log("handling as db connection error");
+    return res.status(500).send({ errors: [{ message: err.reason }] });
   }
 
   res.status(400).send({
-    message: err.message,
+    errors: [{ message: "Something went wrong" }],
   });
 };
