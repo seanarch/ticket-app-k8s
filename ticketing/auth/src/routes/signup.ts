91a312583192b5ce50953aee6d7b8cef704eb946
seanarch <zhuqingyun@outlook.com>
2023-05-03 15:13:56 -0600

    Update email signup validation middleware

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -16,14 +16,13 @@ router.post(
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res.status(400).send(errors.array());
+      throw new Error("Invalid email or password");
     }
 
-    const { email, password } = req.body;
-
     console.log("Creating a user...");
+    throw new Error("Error connecting to database");
 
-    // new User({email, password})
+    res.send({});
   }
 );
 
