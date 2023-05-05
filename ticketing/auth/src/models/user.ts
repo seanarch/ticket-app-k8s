ecb0352df8d25848db51c9c8b097c2bc048f77b9
seanarch <zhuqingyun@outlook.com>
2023-05-04 21:47:08 -0600

    Create user doc interface

--- ticketing/auth/src/models/user.ts
+++ ticketing/auth/src/models/user.ts
@@ -8,6 +8,17 @@ interface UserAttrs {
   password: string;
 }
 
+// an interface that describes the properties that a user model has
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+// an interface that describes the properties that a User document has
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+}
+
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -19,10 +30,10 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model("User", userSchema);
-
-const buildUser = (attrs: UserAttrs) => {
+userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
 
-export { User, buildUser };
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
+
+export { User };
